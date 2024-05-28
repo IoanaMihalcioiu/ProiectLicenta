db75920a9b064f21d7dc5b309d277c6bc63f46b7
IoanaMihalcioiu <mihalcioiu.ioana@yahoo.com>
2024-05-28 21:36:56 +0300

    intro page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./login";
 import Signup from "./signup";
 import Home from './home'
+import Intro from "./intro";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Intro />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/home' element={<Home />} />
---
+++ frontend/src/intro.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './intro.css'; // Vom adăuga stiluri pentru această pagină
+
+const Intro = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/login');
+    };
+
+    return (
+        <div className="homepage-container">
+            <div className="content">
+                <h1>Platformă web de e-learning pentru electronică</h1>
+                <p>Pășește în viitorul tehnologiei: începe aventura ta în lumea electronicii cu platforma noastră de e-learning, unde fiecare clic deschide noi orizonturi ale cunoașterii!</p>
+                <div className="homepage-button-wrapper">
+                    <button className="homepage-button" onClick={handleButtonClick}>Descoperă Lumea Electronicii</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Intro;
